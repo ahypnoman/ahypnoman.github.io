b9bcc2c2bb736d33a89fd3ad5fa4c98bce11cda7
ahypnoman <57266777+ahypnoman@users.noreply.github.com>
2022-08-11 08:28:33 +0100

    IDK what a commit is or how to use it

---
+++ globalButtons.js
@@ -0,0 +1,15 @@
+
+console.log("loaded:globalButtons")
+
+function addButtons() {
+    let body = document.body
+    body.insertAdjacentHTML("afterbegin", "<div class=\"globalButtons\"></div>")
+    let GBDiv = document.getElementsByClassName("globalButtons")[0]
+    if (window.location.pathname !== "/" && window.location.pathname !== "/ahypnoman.github.io/") {
+        GBDiv.insertAdjacentHTML("beforeend", `<a href="./" class="globalButton"><img src="homeIcon.png" width="50" alt="Home"></a>`)
+        const params = new URLSearchParams(document.location.search)
+        if (params.has("from")){}
+    }
+}
+
+addButtons()
---
+++ globalLoader.js
@@ -0,0 +1,23 @@
+window.addEventListener("load", loader)
+
+
+
+function loader(){
+    let head = document.head
+
+    let scripts = ["globalButtons.js"]
+
+    scripts.forEach(scr => {
+        const newScript = document.createElement("script")
+        newScript.src = scr
+        head.appendChild(newScript)
+    })
+    let resources = ["styles.css"]
+
+    resources.forEach(resource => {
+        const newResource = document.createElement("link")
+        newResource.rel = "stylesheet"
+        newResource.href = resource
+        head.appendChild(newResource)
+    })
+}
