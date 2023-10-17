3741993664e239806bedfa77f0962f5a61bc96eb
AHypnoman <57266777+ahypnoman@users.noreply.github.com>
2023-10-17 09:33:49 +0100

    Add javaScratch site and links to it. Fix error in globalButtons.js.

--- globalButtons.js
+++ globalButtons.js
@@ -3,7 +3,7 @@ function addButtons() {
     body.insertAdjacentHTML("afterbegin", "<div class=\"globalButtons\"></div>")
     let GBDiv = document.getElementsByClassName("globalButtons")[0]
     if (window.location.pathname !== "/" && window.location.pathname !== "/ahypnoman.github.io/") {
-        GBDiv.insertAdjacentHTML("beforeend", `<a href="../" class="globalButton"><img src="../images/homeIcon.png" width="50" alt="Home"></a>`)
+        GBDiv.insertAdjacentHTML("beforeend", `<a href="${window.location.host}" class="globalButton"><img src="../images/homeIcon.png" width="50" alt="Home"></a>`)
         const params = new URLSearchParams(document.location.search)
         if (params.has("from")){}
     }
